998c05857e43beb1e2a8b8a9df0d4d05b5724296
xiexuodng <xiexudong@yy.com>
2018-12-28 11:55:28 +0800

    适配android o通知栏

--- framemonitor-android/src/main/java/com/vip001/monitor/utils/ServiceStartUtils.java
+++ framemonitor-android/src/main/java/com/vip001/monitor/utils/ServiceStartUtils.java
@@ -1,10 +1,13 @@
 package com.vip001.monitor.utils;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by xxd on 2018/12/27
@@ -15,15 +18,32 @@ public class ServiceStartUtils {
 
         if (keepLiveService != null) {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                keepLiveService.startForeground(foregroundPushId, new Notification());
+                keepLiveService.startForeground(foregroundPushId, generateNotification(keepLiveService.getApplication()));
             } else {
-                keepLiveService.startForeground(foregroundPushId, new Notification());
+                keepLiveService.startForeground(foregroundPushId, generateNotification(keepLiveService.getApplication()));
                 if (innerService != null) {
-                    innerService.startForeground(foregroundPushId, new Notification());
+                    innerService.startForeground(foregroundPushId, generateNotification(keepLiveService.getApplication()));
                     innerService.stopSelf();
                 }
             }
         }
+
+
+    }
+
+    private static Notification generateNotification(Context context) {
+        Notification notification = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(context.getPackageName(), context.getPackageName(), NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+            notification = new NotificationCompat.Builder(context, context.getPackageName())
+                    .setChannelId(context.getPackageName())
+                    .build();
+        } else {
+            notification = new Notification();
+        }
+        return notification;
     }
 
     public static boolean startService(Context context, Intent intent) {
